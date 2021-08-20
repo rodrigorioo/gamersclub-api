2db8ef07ef7640a8f4adcbba2f66213758a5e802
Rodrigo Rio <rodrigo.rio@outlook.com.ar>
2021-08-20 20:08:55 -0300

    User agent

--- src/GC.js
+++ src/GC.js
@@ -35,6 +35,7 @@ class GC {
                     '--single-process',
                     '--no-zygote',
                     '--no-sandbox',
+                    '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
                 ],
             });
 
